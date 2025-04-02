f7aa8e3a849acc0fb3ccaa43a63ad7f0e570329c
youssef elhoubi <elhoubiyoussef@gmail.com>
2025-04-02 22:24:33 +0000

    fineshed the making of the add to watch list

--- TMPback/app/Http/Controllers/watchlist.php
+++ TMPback/app/Http/Controllers/watchlist.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Validation\ValidationException;
+use App\Models\watchlist as WatchListM;
 
 class watchlist extends Controller
 {
@@ -13,9 +14,20 @@ class watchlist extends Controller
         try {
             $user_Id = $request->user_id;
             $validation = $request->validate([
-
+                "product_id" => "required|numirec",
             ]);
             $productId = $request->product_id;
+            WatchListM::create([
+                "user_id" => $user_Id,
+                "product_id" => $productId
+            ]);
+            return response()->json(
+                [
+                    "status" => "success",
+                    "message" => "a new item have been added to your watch list"
+                ],
+                Response::HTTP_CREATED
+            );
         } catch (ValidationException $e) {
             return response()->json([
                 "error" => $e->getMessage()
