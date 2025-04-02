ba9ad6c1a3fb82d0c29e0fa453e1e49bcb5f020e
youssef elhoubi <elhoubiyoussef@gmail.com>
2025-04-02 20:35:27 +0000

    working on makeing the wach list

--- TMPback/app/Http/Controllers/watchlist.php
+++ TMPback/app/Http/Controllers/watchlist.php
@@ -3,10 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Validation\ValidationException;
 
 class watchlist extends Controller
 {
-    public function addToWachList(Request $request){
+    public function addToWachList(Request $request)
+    {
+        try {
+            $user_Id = $request->user_id;
+            $validation = $request->validate([
 
+            ]);
+            $productId = $request->product_id;
+        } catch (ValidationException $e) {
+            return response()->json([
+                "error" => $e->getMessage()
+            ], Response::HTTP_BAD_REQUEST);
+        }
     }
 }
