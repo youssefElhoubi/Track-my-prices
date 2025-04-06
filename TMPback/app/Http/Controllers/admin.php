ee69448a4e2d775c9fc3fc146cfa0534e4168f87
youssef elhoubi <elhoubiyoussef@gmail.com>
2025-04-06 15:18:03 +0100

    made a funcitno to get all the products with there users

--- TMPback/app/Http/Controllers/admin.php
+++ TMPback/app/Http/Controllers/admin.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\products;
+use Symfony\Component\HttpFoundation\Response;
 
 class admin extends Controller
 {
-    //
+    public function products(Request $request)
+    {
+        $products = Products::with('owner')->get();
+
+        return response()->json(
+            [
+                'success' => true,
+                'data' => $products
+            ],
+            Response::HTTP_OK
+        );
+    }
 }
--- TMPback/app/Models/products.php
+++ TMPback/app/Models/products.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\products_history;
+use App\Models\User;
+use Symfony\Component\HttpFoundation\Response;
 
 class products extends Model
 {
@@ -17,5 +19,8 @@ class products extends Model
     ];
     public function hestory() {
         return $this->hasMany(products_history::class,"product_id");
+    }
+    public function owner(){
+        return $this->hasOne(User::class,"user_id");
     }
 }
