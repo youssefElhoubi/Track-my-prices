782ce45d917841833ed4e3c4f4df5d72557c5363
youssefelhoubi <elhoubiyoussef@gmail.com>
2025-04-20 20:21:09 +0100

    added an image url to the user db

---
+++ TMPback/database/migrations/2025_04_20_164451_add_column_image_to_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->text("Image_url")->default("https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn("Image_url");
+        });
+    }
+};
