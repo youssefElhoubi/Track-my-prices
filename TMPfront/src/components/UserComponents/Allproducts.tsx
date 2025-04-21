77166e9334b5e4cdf614e92801a28ceca3196f0c
youssefelhoubi <elhoubiyoussef@gmail.com>
2025-04-21 16:45:22 +0100

    added a method to get the totale numebr of the products

--- TMPfront/src/components/UserComponents/Allproducts.tsx
+++ TMPfront/src/components/UserComponents/Allproducts.tsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axiosConfig from '../../api/axiosConfig';
 
 const Allproducts: React.FC = () => {
+    const [total, settotal] = useState(0);
+    useEffect(() => {
+        const total = async () => {
+            try {
+                const Token = localStorage.getItem("token");
+                const totalProducts = await axiosConfig.get("product/total", {
+                    headers: {
+                        Authorization: Token
+                    }
+                });
+                settotal(totalProducts.data.totale);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        total();
+    }, [])
+
     return (
         <>
             <div className="flex justify-between py-7">
                 <h1 className='text-3xl font-black'>
                     All Tracked Products
                 </h1>
-                <p>1,234 Products Found</p>
+                <p>{total} Products Found</p>
             </div>
         </>
     )
