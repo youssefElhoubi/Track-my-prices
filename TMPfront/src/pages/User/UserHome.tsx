27ca0698c4e7741c31ced5be3845022bfde168d1
youssefelhoubi <elhoubiyoussef@gmail.com>
2025-04-22 14:05:26 +0100

    working on the thing of the user home

--- TMPfront/src/pages/User/UserHome.tsx
+++ TMPfront/src/pages/User/UserHome.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../../components/UserComponents/Nav';
 import Allproducts from '../../components/UserComponents/Allproducts';
+import ProductCard from '../../components/product/ProductCard';
 
 const UserHome: React.FC = () => {
     return (
@@ -8,6 +9,14 @@ const UserHome: React.FC = () => {
             <Navbar />
             <div className="px-20">
             <Allproducts/>
+            </div>
+            {/* products main content */}
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 px-12 '>
+                <ProductCard/>
+                <ProductCard/>
+                <ProductCard/>
+                <ProductCard/>
+                <ProductCard/>
             </div>
         </>
     )
