85fca1a0efe65664fb361ab39dccccbd0223a3cd
youssefelhoubi <elhoubiyoussef@gmail.com>
2025-04-30 16:35:20 +0100

    added a change to the icone file to aplay the class of surtent icone

--- TMPfront/src/components/common/Iconse.tsx
+++ TMPfront/src/components/common/Iconse.tsx
@@ -329,7 +329,7 @@ export function User({ className }: { className?: string }) {
 }
 export function DownArrow({ className }: { className?: string }) {
     return (
-        <svg className="h-4 w-4 text-gray-500" viewBox="0 0 20 20" fill="currentColor" >
+        <svg className={`h-4 w-4 text-gray-500${className}`} viewBox="0 0 20 20" fill="currentColor"  >
             <path
                 fillRule="evenodd"
                 d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
