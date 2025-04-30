691522d35c309e90b6201c0fac75b53fc39de905
youssefelhoubi <elhoubiyoussef@gmail.com>
2025-04-30 17:27:36 +0100

    finched working on the user watchlist stor now I'm going to test it

--- TMPfront/src/stor/WatchlistStpor.tsx
+++ TMPfront/src/stor/WatchlistStpor.tsx
@@ -1,3 +1,30 @@
-import {create} from "zustand";
+import { create } from "zustand";
+import axiosConfig from "../api/axiosConfig";
 
 
+type WatchlistState = {
+    response: any;
+    isLoading: boolean;
+    error: any;
+    fetchWatchLst: (token: string) => void
+};
+
+const userWatchlist = create<WatchlistState>((set) => ({
+    response: null,
+    isLoading: false,
+    error: null,
+    fetchWatchLst: async (token) => {
+        set({ error: null, isLoading: true })
+        try {
+            const result = await axiosConfig.get("watchlist", {
+                headers: {
+                    Authorization: token
+                }
+            })
+            set({ response: result.data, isLoading: false });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}));
+export default userWatchlist;
