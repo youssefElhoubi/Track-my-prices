94afa4fea2683886a7855bfec5c10343993f259c
youssefelhoubi <elhoubiyoussef@gmail.com>
2025-05-01 19:21:53 +0100

    working on shoing the watch list

--- TMPfront/src/App.tsx
+++ TMPfront/src/App.tsx
@@ -10,6 +10,7 @@ import ResetPassword from "./pages/ResetPassword";
 import NewPasswordReset from "./pages/NewPasswordReset";
 import ProductDetailPage from "./pages/User/ProducrDetailes";
 import ProductTracker from "./pages/User/Myprodcuts";
+import MyWatchlist from "./pages/User/MyWatchlist";
 
 
 
@@ -29,7 +30,8 @@ function App() {
           <Route path="/user" element={<Privet Componnet={UserHome} />} />
           <Route path="/user/product/add" element={<Privet Componnet={TraceNewProduct} />} />
           <Route path="/user/product/:id" element={<Privet Componnet={ProductDetailPage} />} />
-          <Route path="/test" element={<ProductTracker />} />
+          <Route path="/user/products" element={<Privet Componnet={ProductTracker} />} />
+          <Route path="/test" element={<MyWatchlist />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
--- TMPfront/src/pages/User/MyWatchlist.tsx
+++ TMPfront/src/pages/User/MyWatchlist.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../components/UserComponents/Nav'
 import Footer from '../../components/lanign components/Footer'
+import userWatchlist from '../../stor/WatchlistStpor';
 
 const MyWatchlist: React.FC = () => {
+    const { error, fetchWatchLst, isLoading, response } = userWatchlist()
+    const [WatchList, setWatchList] = useState();
     return (
         <>
-        <Navbar/>
-        
-        <Footer/>
+            <Navbar />
+            <Footer />
         </>
     )
 }
