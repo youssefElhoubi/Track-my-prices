2b2c67817bb0d7714a11e06c3c932423cb25cfe0
youssefelhoubi <elhoubiyoussef@gmail.com>
2025-05-05 14:41:20 +0100

    deleted some lines to not take the admine to the page of the product when clikcing on the product

--- TMPfront/src/components/AdmineComponents/AdminProductCard.tsx
+++ TMPfront/src/components/AdmineComponents/AdminProductCard.tsx
@@ -38,29 +38,27 @@ const AdminProductCard: React.FC<product> = ({ id, url, name, productImage, cure
 
 
     return (
-        <a href={`/user/product/${id}`}>
-            <div className="md:w-72 rounded-xl bg-white shadow-sm border border-gray-100 overflow-hidden">
-                <div className="p-4 relative">
-                    <div className="bg-gray-100 rounded-lg mb-4 flex items-center justify-center">
-                        <img
-                            src={productImage}
-                            alt="Wireless Headphones"
-                            className="h-48 object-contain"
-                        />
-                    </div>
-                    <div className="space-y-2">
-                        <h3 className="font-medium text-lg text-gray-900">{name}</h3>
-                        <div className="flex items-center justify-between">
+        <div className="md:w-72 rounded-xl bg-white shadow-sm border border-gray-100 overflow-hidden">
+            <div className="p-4 relative">
+                <div className="bg-gray-100 rounded-lg mb-4 flex items-center justify-center">
+                    <img
+                        src={productImage}
+                        alt="Wireless Headphones"
+                        className="h-48 object-contain"
+                    />
+                </div>
+                <div className="space-y-2">
+                    <h3 className="font-medium text-lg text-gray-900">{name}</h3>
+                    <div className="flex items-center justify-between">
                         <p className="text-gray-500 text-sm">{platformName}</p>
                         <div className="flex">
-                            <img src="https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_640.png" alt="userimg" className="w-5"/>
-                            <TrashIcon className="text-red-400"/>
-                        </div>
+                            <img src="https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_640.png" alt="userimg" className="w-5" />
+                            <TrashIcon className="text-red-400" onClick={() => { }} />
                         </div>
                     </div>
                 </div>
             </div>
-        </a>
+        </div>
     )
 }
 
